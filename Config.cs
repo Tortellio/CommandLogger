a62eff4a2df015afcf221e09d4f14db3e02b94b0
Coolpuppy24 <pat.pvp.unturned@gmail.com>
2017-04-18 15:55:06 -0500

    Added Config w/ more options

--- Config.cs
+++ Config.cs
@@ -1,11 +1,44 @@
-﻿using System;
+﻿using Rocket.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace coolpuppy24.commandlogger
 {
-    class Config
+    public class Config : IRocketPluginConfiguration
     {
+        public bool LogVanishCommand;
+        public bool LogICommand;
+        public bool LogGiveCommand;
+        public bool LogGodCommand;
+        public bool LogVCommand;
+        public bool LogVehicleCommand;
+        public bool LogItemCommand;
+        public bool LogTPCommand;
+        public bool LogTeleportCommand;
+        public bool LogSpyCommand;
+        public bool LogTPHereCommand;
+        public bool LogInvestigateCommand;
+        public bool LogBanCommand;
+        public bool LogKickCommand;
+
+        public void LoadDefaults()
+        {
+            LogVanishCommand = true;
+            LogICommand = true;
+            LogGiveCommand = true;
+            LogGodCommand = true;
+            LogVCommand = true;
+            LogVehicleCommand = true;
+            LogItemCommand = true;
+            LogTPCommand = true;
+            LogTeleportCommand = true;
+            LogSpyCommand = true;
+            LogTPHereCommand = true;
+            LogInvestigateCommand = true;
+            LogBanCommand = true;
+            LogKickCommand = true;
+        }
     }
 }
--- Main.cs
+++ Main.cs
@@ -14,9 +14,9 @@ using System.Collections.Generic;
 using Rocket.Unturned.Events;
 using System.IO;
 
-namespace coolpuppy24.plugintemplate
+namespace coolpuppy24.commandlogger
 {
-    public class Main : RocketPlugin
+    public class Main : RocketPlugin<Config>
     {
 
         public string CommandLogger = System.IO.Directory.GetCurrentDirectory() + @"\Plugins\CommandLogger\CommandLog.txt";
@@ -31,65 +31,79 @@ namespace coolpuppy24.plugintemplate
 
             UnturnedPlayerEvents.OnPlayerChatted += UnturnedPlayerEvents_OnPlayerChatted;
             Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Made by: Coolpuppy24");
-
+            Rocket.Core.Logging.Logger.LogWarning("----------------------------------------------");
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log Vanish Command: " + Configuration.Instance.LogVanishCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log I Command: " + Configuration.Instance.LogICommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log Item Command: " + Configuration.Instance.LogItemCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log Give Command: " + Configuration.Instance.LogGiveCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log God Command: " + Configuration.Instance.LogGodCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log V Command: " + Configuration.Instance.LogVCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log Vehicle Command: " + Configuration.Instance.LogVehicleCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log TP Command: " + Configuration.Instance.LogTPCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log Teleport Command: " + Configuration.Instance.LogTeleportCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log Spy Command: " + Configuration.Instance.LogVanishCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log TPHere Command: " + Configuration.Instance.LogTPHereCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log Investigate Command: " + Configuration.Instance.LogInvestigateCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log Ban Command: " + Configuration.Instance.LogBanCommand);
+            Rocket.Core.Logging.Logger.LogWarning("[CommandLogger] Log Kick Command: " + Configuration.Instance.LogKickCommand);
             Rocket.Core.Logging.Logger.Log("[CommandLogger] Successfully Loaded!");
 
     }
 
         private void UnturnedPlayerEvents_OnPlayerChatted(UnturnedPlayer player, ref Color color, string message, EChatMode chatMode, ref bool cancel)
         {
-            if (message.StartsWith("/vanish")) {
+            if (message.StartsWith("/vanish") && Configuration.Instance.LogVanishCommand) {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/i"))
+            if (message.StartsWith("/i") && Configuration.Instance.LogVanishCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/give"))
+            if (message.StartsWith("/give") && Configuration.Instance.LogGiveCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/god"))
+            if (message.StartsWith("/god") && Configuration.Instance.LogGodCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/v"))
+            if (message.StartsWith("/v") && Configuration.Instance.LogVCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/vehicle"))
+            if (message.StartsWith("/vehicle") && Configuration.Instance.LogVehicleCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/item"))
+            if (message.StartsWith("/item") && Configuration.Instance.LogItemCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/tp"))
+            if (message.StartsWith("/tp") && Configuration.Instance.LogTPCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/teleport"))
+            if (message.StartsWith("/teleport") && Configuration.Instance.LogTeleportCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/spy"))
+            if (message.StartsWith("/spy") && Configuration.Instance.LogSpyCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/tphere"))
+            if (message.StartsWith("/tphere") && Configuration.Instance.LogTPHereCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/investigate"))
+            if (message.StartsWith("/investigate") && Configuration.Instance.LogInvestigateCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/ban"))
+            if (message.StartsWith("/ban") && Configuration.Instance.LogBanCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
-            if (message.StartsWith("/kick"))
+            if (message.StartsWith("/kick") && Configuration.Instance.LogKickCommand)
             {
                 File.AppendAllText(CommandLogger, "[" + DateTime.Now + "] " + player.CharacterName + " (" + player.CSteamID + ")" + ": \"" + message + "\"" + System.Environment.NewLine);
             }
